70de220ed93bcd9c09e40ca78a22c9c864ee608e
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2022-07-14 16:52:40 +0200

    Added secret key output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use openssl::{pkey::PKey};
+use std::{path::{PathBuf, Path}, ffi::OsString};
+
+use openssl::{pkey::PKey, nid::Nid};
 use clap::Parser;
 mod beam_id;
 mod errors;
@@ -11,41 +13,64 @@ struct Args {
     #[clap(long, env, value_parser)]
     proxy_id: String,
 
+    /// Directory for private key storage
+    #[clap(long, env, value_parser, default_value="/etc/bridgehead/secrets")]
+    output_path: PathBuf,
+
+
+    #[clap(long, env, value_parser, default_value="admin@samply.beam.dkfz.de")]
+    admin_email: String,
+
+    #[clap(long, env, value_parser, default_value="false")]
+    overwrite: bool,
+
     // Broker Domain
     //#[clap(long, env, value_parser)]
     //broker_url: String,
 }
 
-#[allow(dead_code)]
-pub(crate) struct Config {
-    proxy_id: beam_id::ProxyId,
-}
 fn main() {
     let args = Args::parse();
-    let id = beam_id::ProxyId::new("proxy.id").unwrap();
-    generate_priv_key_and_csr(&id).unwrap();
+    let id = beam_id::ProxyId::new(&args.proxy_id).unwrap();
+    println!("Welcome to the Samply.Beam enrollment companion app.");
+    println!("This application generates");
+    let outpath: OsString= args.output_path.clone().into_os_string();
+    let outpath = outpath.to_string_lossy();
+    println!("\ta) a secret key. This file is automatically saved in the folder {} and must not be shared," , outpath);
+    println!("\tb) a certificate sign request. This is output sent to the administrator of the central broker via email to: {}.", args.admin_email);
+    let (priv_key, csr) = generate_priv_key_and_csr(&id).unwrap();
+    write_priv_key(priv_key, id, &args.output_path, args.overwrite).unwrap();
+    println!("File written.");
+    println!("Please send the following text block to {}:", args.admin_email);
+    println!("{}", String::from_utf8(csr).unwrap());
+
 }
 
-fn generate_priv_key_and_csr(proxy_id: &beam_id::ProxyId) -> anyhow::Result<()> {
-    println!("Generate new private key");
+fn generate_priv_key_and_csr(proxy_id: &beam_id::ProxyId) -> anyhow::Result<(Vec<u8>, Vec<u8>)> {
+    println!("Generating new private key");
     let rsa = PKey::from_rsa(openssl::rsa::Rsa::generate(2048)?)?;
     let private_key = &rsa.private_key_to_pem_pkcs8()?;
-    let public_key = &rsa.public_key_to_pem()?;
-    println!("Build name");
+    println!("Generating Certificate Sign Request");
     let mut name_builder = openssl::x509::X509Name::builder()?;
-    name_builder.append_entry_by_text("CN", proxy_id.value())?;
-    name_builder.append_entry_by_text("C", "DE")?;
-    println!("Build CSR");
+    name_builder.append_entry_by_nid(Nid::COMMONNAME, proxy_id.value())?;
+    name_builder.append_entry_by_nid(Nid::COUNTRYNAME, "DE")?;
     let mut csr_builder = openssl::x509::X509Req::builder()?;
     csr_builder.set_pubkey(rsa.as_ref())?;
     csr_builder.set_subject_name(&name_builder.build())?;
+    csr_builder.sign(rsa.as_ref(), openssl::hash::MessageDigest::sha256())?;
     let csr = csr_builder.build().to_pem()?;
 
-    println!("CSR: {}", String::from_utf8(csr).unwrap());
-    println!("Private: {}", String::from_utf8_lossy(private_key));
-    println!("Public: {}", String::from_utf8_lossy(public_key));
-
-    println!("Please execute  openssl req -nodes -new -newkey rsa:2048 -sha256 -keyout secret_key.pem -out <ProxyId>_csr.pem -subj '/CN=<ProxyId>/C=DE'");
-    //Err(SamplyBeamError::SignEncryptError(String::from("Cannot generate CSR and private Key")))
-    Ok(())
+    Ok((private_key.clone(),csr))
 }
+ fn write_priv_key(priv_key: Vec<u8>, proxy_id: ProxyId, path: &Path, overwrite: bool) -> anyhow::Result<()>{
+    let proxy = proxy_id.value().split('.').map(|v| String::from(v)).collect::<Vec<String>>();
+    let filename_string = proxy[0].to_owned() + "_secretkey";
+    let filename = path.clone().with_file_name(&filename_string).with_extension("csr");
+    if filename.exists() && !overwrite{
+        eprintln!("File {} already exists. For overwriting set --overwrite flag.", filename.into_os_string().to_string_lossy());
+        std::process::exit(2);
+    }
+    println!("filename is: {:?}",filename_string);
+    std::fs::write(filename, priv_key)?;
+    Ok(())
+ }
